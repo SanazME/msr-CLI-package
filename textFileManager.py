e258d73456653239bedd477d2ce5ed9a46fa8c81
Sanaz <sanaz.azh@gmail.com>
2020-08-09 12:09:41 -0400

    add a dummy file for creating a text file manager cli

---
+++ textFileManager.py
@@ -0,0 +1,34 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='A text file manager with the following flags.')
+    parser.add_argument('-r', '--read', metavar='file_name', nargs=1,
+                        type=open, help='Opens and reads the specified text file.')
+    parser.add_argument('-s', '--show', metavar='path', nargs=1,
+                        help='Show all the text files on specified directory path. Type \'.\' for current directory')
+    parser.add_argument('-d', '--delete', metavar='file_name', nargs=1,
+                        help='Deletes the specified text file.')
+    parser.add_argument('-c', '--copy', metavar=('file1', 'file2'), nargs=2,
+                        help='Copy file1 contents to file2 Warning: file2 will get overwritten.')
+    parser.add_argument('--rename', metavar=('old_name', 'new_name'), nargs=2,
+                        help='Renames the specified file to a new name.')
+    
+    args = parser.parse_args()
+    
+    if args.read != None:
+        read(args)
+    elif args.show != None:
+        show(args)
+    elif args.delete != None:
+        delete(args)
+    elif args.copy != None:
+        copy(args)
+    elif args.rename != None:
+        rename(args)
+
+
+if __name__ == "__main__":
+    # calling the main function
+    main()
