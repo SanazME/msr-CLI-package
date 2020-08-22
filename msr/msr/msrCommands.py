8952c6c4b5fceab57b83a09d9172694d48e7973f
Sanaz <sanaz.azh@gmail.com>
2020-08-22 09:57:51 -0400

    add database to gitignore and cleanup code

--- msr/msr/msrCommands.py
+++ msr/msr/msrCommands.py
@@ -1,7 +1,12 @@
 import argparse
 import validators
 import sys
+import sqlite3
 from ._version import __version__
+
+# install package locally:
+# python setup.py sdist bdist_wheel
+# pip install -e .
 
 
 def version(args):
@@ -13,10 +18,57 @@ def register(args):
 
     if isUrlValid:
         url = args.register[0]
-        print(url)
+
+        # insert to database
+        handleDB(url)
+
         return
     print('wrong url')
     return sys.exit(1)
+
+
+# create a new database
+def handleDB(url):
+
+    try:
+        # create or connect to existing database
+        connectdb = sqlite3.connect('urlLinks.db')
+        cursor = connectdb.cursor()
+
+        # check if table exists
+        tableExist = isTableExists(cursor, 'urls')
+
+        if tableExist: 
+            print('Table already exists')
+        else:
+            # only need to execute once???
+            cursor.execute(''' CREATE TABLE urls
+            (
+            ADDRESS     VARCHAR(2083) PRIMARY KEY NOT NULL); ''')
+            print('Table is created successfully!')
+
+        # insert to table
+        cursor.execute('INSERT INTO urls VALUES (?)', (url,))
+
+        # save (commint) the changes
+        connectdb.commit()
+        print('Record created successfully!')
+
+    except sqlite3.Error as error:
+        print("Failed to handle DB with error: ", error)
+    
+    finally:
+        if (connectdb):
+            # close the db connection
+            connectdb.close()
+            print('Connection to db is closed.')
+
+
+def isTableExists(cursor, tableName):
+    cursor.execute(
+        ''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name=?''', (tableName,))
+
+    return cursor.fetchone()[0] == 1      
 
 
 def main():
